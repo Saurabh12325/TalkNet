9f64a686d52936d9347052d7914f7b0f3137dd6a
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-07-08 00:31:34 +0530

    first commit

---
+++ src/main/java/com/Bot/Talknet/TalkNetApplication.java
@@ -0,0 +1,13 @@
+package com.Bot.Talknet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bot/Talknet/TalkNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bot.Talknet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
